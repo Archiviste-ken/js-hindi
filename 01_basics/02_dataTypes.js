75ecd5f5dedf1115db3d6199fce72d3d1c7ec194
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-08-17 07:34:03 +0000

    container confg

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,35 @@
 "use strict"; // treat all JS code as newer version.
 
-alert(3 + 3)
+// alert(3 + 3) we are using nodejs,not browser
+
+console.log(3 
+    + 
+    3); // code readibility should be high and not like this
+
+console.log("Hitesh");
+
+
+
+let name ="hitesh" // string value
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => no valuue is assigned to the variable
+// symbol => unique 
+
+//object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined);// undefined
+
+
+
+
+
