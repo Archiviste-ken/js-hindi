f8af15e1cd52376dfc365f73d9e65db0e670911e
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-08-29 06:06:03 +0000

    comparisons of datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,35 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2 < 1);
+//console.log(2==1);
+//console.log(2!=1);
+
+console.log("2" > 1);
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);      //don't ever use these types of conversion or comparisions , they cause confusion.
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// === this is strict check , it basically checks all the elements of the relation , like data type etc.
+
+console.log( 2 === 2); // true 
+
+//console.log("2" === 2); false or error.
+
+
+
+
+
+
+
+
+
+
+
+
