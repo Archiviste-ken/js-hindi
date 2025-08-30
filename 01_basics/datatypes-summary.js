95b56f39aeecf01cabe85d72acc0c0bfc638cffe
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-08-30 16:16:20 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+// # Primitive 
+
+// 7 types : String , Number ,Boolean ,Null ,undefined ,Symbol , BIGInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+
+// reference (non primitive)
+
+//Array , Objects , Functions 
