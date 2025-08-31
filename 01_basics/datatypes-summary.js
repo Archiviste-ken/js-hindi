b2bd2571a039221beac85f6d78185ba94f6cea5e
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-08-31 19:36:37 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -10,8 +10,34 @@ const outsideTemp = null
 let userEmail;
 
 const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(id ===anotherID);
+
+// const bigNumber =4167642367624652624642n 
+
 
 
 // reference (non primitive)
 
 //Array , Objects , Functions 
+
+const heros = ["shaktiman" , "naagraj" , "doga"]
+
+let myObj ={
+
+    name: "shreyesh",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof bigNumber);
+
+
+
+    
+
