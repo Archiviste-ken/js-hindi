a4175946fc7b3b51ad7eddf1c258bd4236f85ef8
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-06 14:25:56 +0000

    nums and math

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,10 +1,10 @@
 const name = "shreyesh"
 const repoCount = 50
 
-console.log(name + repoCount + " Value");
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(name + repoCount + " Value"); //  don't use it like this
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);// write this way
 
-const gameName = new String('shreyeshhc')
+const gameName = new String('hitesh-hc-com')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -21,17 +21,19 @@ console.log(newString);
 const anotherString = gameName.substring(-8,4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh   "
-console.log(newStringOne );
-console.log(newString.trim());
+// const newStringOne = "   hitesh   "
+// console.log(newStringOne );
+// console.log(newString.trim());
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20'),'-')
+console.log(url.replace('%20', '-'))
 
 console.log(url.includes('sundar'))
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-'));  
+
+// read mdn,dimension repor t and learn more about the strings
 
 
 
