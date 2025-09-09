c55da5e1954eee1c32ca62c88e83742cbc58c323
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-09 14:57:25 +0000

    arrays

---
+++ 02_basics/02_array.js
@@ -0,0 +1,14 @@
+const marvel_heroes = ["thor","Ironman","spiderman"]
+const dc_heroes = ["superman", "flash" , "batman"]
+
+//marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][0]);
+
+marvel_heroes.concat(dc_heroes) //concatenation of arrays in js
+console.log(marvel_heroes);
+
+// so hence it is clear as a day that push and concatenation gives same output.
+
+
