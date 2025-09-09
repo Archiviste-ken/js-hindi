8623d7890e003415bac93b2cbd12d8cb899e9d8a
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-09 18:49:05 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,22 @@
+// Objects can be created using the Object() constructor or the object initializer / literal syntax.
+
+// Objects = It is used to store various keyed collections and more complex entities.
+
+// singleton
+
+// objects literals
+//Object.create
+
+const JsUser = {
+    name  : "Shreyesh",
+    age  : 20,
+    location : "Jaipur",
+    email : "shreyesh@google.com",
+    isloggedIn :false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+
+
