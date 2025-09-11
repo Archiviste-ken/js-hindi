e0bdd57f41a392416b2b685b95093e2b9bcf7299
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-11 16:43:03 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -52,17 +52,52 @@ const users = [
 
 
 ]
-console.log(users[1].email)
-console.log(tinderUser);
+// console.log(users[1].email);
+// console.log(tinderUser
 
-console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser));
 
-console.log(Object.values(tinderUser));
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+const course =  {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+// const {} = courseInstructor
+
+const {courseInstructor: instructor} = course 
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar( company = "hitesh")
+
+// {
+//     name: "hitesh",
+//     coursename:"js in hindi",
+//     price: "free"
+
+// }
+
+{
+    {},
+    {},                                         // json formatter
+    {}
+}
+
+ 
 
 
 
