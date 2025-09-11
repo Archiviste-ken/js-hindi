0c60ee51c098eb3613034e44491d317413996eb0
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-11 08:23:45 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,3 +1,70 @@
 //const tinderUser = new Object() // SINGLETON
 const tinderUser ={} // same output as above // NON SINGLETON
+
+tinderUser.id = "123abc"
+tinderUser.name ="Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname:  {
+        userfullname:{
+            firstname: "shreyesh",
+            lastname: "pachpor"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a", 2:"b"} 
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"} 
+
+
+// const obj3 ={ obj1, obj2   }
+// const obj3 = Object.assign({},obj1,obj2,obj4) //The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
+
+const obj3 = {...obj1,...obj2,...obj4}  // spread operator is latest and more preferable to use.
+
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email:" h@gamil.com"
+    },
+     {
+        id: 1,
+        email:" h@gamil.com"
+    },
+     {
+        id: 1,
+        email:" h@gamil.com"
+    },
+ {
+        id: 1,
+        email:" h@gamil.com"
+    },
+
+
+]
+console.log(users[1].email)
 console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
+console.log(Object.values(tinderUser));
+
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+
