0f3ad6b77097717050517f48bc11fe2a48d3ea9e
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-12 04:53:50 +0000

    function in object,array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -70,4 +70,39 @@ const result = addTwoNumbers(3 , 5)
 // console.log("Return: ", return);
 
 
-  console.log(loginUserMessage("hitesh"));
+//   console.log(loginUserMessage("hitesh"));
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+
+
+function calculateCartPrice(val1,val2, ...num1){  // ... is  rest operator
+   return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function  handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is  ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100.600]
+
+function returnSecondvalue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondvalue(myNewArray));
+console.log(returnSecondvalue([200,400,500,1000]));
+
