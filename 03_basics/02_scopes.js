7192985249a13db5ff9835ce2547fd98486de138
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-13 03:52:23 +0000

    hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,15 +6,61 @@ if (true){
     console.log("INNER: ", a);  // block scope  
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+//for (let i = 0; i < array.length; i++) {}
+  //  const element = array[i];
     
-}
+//}
 
 console.log(a);
 // console.log(b);
 // console.log(c);  // c is getting printed even when is it within the if statement , indicates the scope issue.
 
 // avoid the usage of var due to the obvious scope issue.
+
+
+function one(){
+    const username ="hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    
+    //console.log(website);
+
+    two()
+    
+}
+
+//one()
+
+if(true){
+    const username ="hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+        console.log(username + website);   
+    
+    } 
+       //console.log(website);    // error due to scope limits
+}
+//console.log(username);  // error due to the scope limits
+
+
+//++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++//
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+console.log(addTwo(5))               // hoisting
+
+ const addTwo = function(num){
+    return num + 2
+ }
+
 
 
