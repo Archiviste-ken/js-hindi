ff41017de5140d12d2173a0bfa93dee005053819
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-14 05:06:31 +0000

    arrays

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -18,9 +18,41 @@ const user = {    // this thing is known as object.
 
 //  console.log(this);
 
-function one(){
-    console.log(this);
-    
+// function one(){
+//      let username = "hitesh"
+//     console.log(this.username;  //  this is only working in objects and not in functions.    
+// }
+
+// one()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);    
+// }
+
+const chai = () => {             //   ARROW
+    let username = "hitesh"
+    console.log(this);    
 }
 
-one()
+//chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// if we use curly braces then we gotta use return
+// explicit return
+
+// const addTwo = (num1, num2) =>  num1 + num2   // implicit  return
+// const addTwo = (num1, num2) =>  (num1 + num2)   // implicit  return
+// if we use parenthesis then no need to use return
+
+const addTwo = (num1, num2) =>  ({username: "shreyesh"})
+
+console.log(addTwo(3, 4));
+
+
+// const myArray = [2,,5,3.8]
+
+// myArray.forEach (() => ())
+
