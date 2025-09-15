e8912b1d31aec949051dd78e467c0c086268e038
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-15 16:12:09 +0000

    switch

---
+++ 04_control_folder/one.js
@@ -0,0 +1,74 @@
+// if
+
+const isUserLoggedIN = true
+const temperature = 41
+  
+// if(2 == "2"){ // == is  normal check , it does'nt check the data type in both the sides
+//     console.log("atomic");
+    
+// }
+//  if(temperature < 50){
+//     console.log("less than 50");
+    
+//  } else {
+//     console.log("temperature is greater than 50");
+    
+//  }
+
+
+ 
+
+  
+// if(2 === "2"){ // === is  strict check , it check the data type in both the sides
+//     console.log("atomic");
+    
+// }
+
+//2<=2
+
+//<,>,<= , >= , == , != , ===, !==
+
+// const score = 200
+
+// if(score > 100) {
+
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+//     console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+// if (balance > 500)  console.log("test") ,console.log("test2"); very very immature code the code readibility should be top notch.
+
+// if(balance < 500){
+//     console.log("less than 500");   
+// } else if (balance < 750){
+//     console.log("balance < 750");
+    
+// }  else if(balance < 900){
+
+//     console.log("less than 750");
+    
+// } else{
+//     console.log("less than 1200");
+
+// }
+
+
+const UserLoggedIN = true
+const debitCard = true
+const loggedINfromGoogle = false
+const loggedINfromEmail = true
+
+if (UserLoggedIN && debitCard && 2==2 ) {
+    console.log("Allow to buy course");
+}
+
+if (loggedINfromGoogle || loggedINfromEmail  || guestUser){
+    console.log("User Logged in");   
+}
+
+
