cf2097fca0aaf402441de33a92acb00921153583
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-17 02:54:57 +0000

    truthy,falsy values

--- 04_control_folder/truthy.js
+++ 04_control_folder/truthy.js
@@ -32,7 +32,7 @@ let val1;
 // val1 = 5 ?? 10
 // val1 =  null ?? 10
 // val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20    // NC operator(??) here the very assigns first value.
+val1 = null ?? 10 ?? 20    // NC operator(??) here assigns first value.
 
 
 console.log(val1);
