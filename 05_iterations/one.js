365e4f9d84d26c11c139d1115ca2fdf759ee1781
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-17 03:36:55 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,16 @@
+// for
+
+// for (let index = 0; index <= 10; index++) {
+//     // const element = index;  
+//     console.log(index);
+// }
+
+ //console.log(element);  // out of scope can't access the variable outside the for loop or function
+
+
+ for (let i = 0; i <= 10; i++) {
+    const element = i;    
+ }
+ 
+
+
