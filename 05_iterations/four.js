960e70beb0207e2fca8fa7482566936fc9654db7
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-18 11:45:20 +0000

    for of and for in

---
+++ 05_iterations/four.js
@@ -0,0 +1,14 @@
+const myObject = { for printing 
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift:"swift by apple"
+}
+
+for (const key in object) {
+    if (!Object.hasOwn(object, key)) continue;
+    
+    const element = object[key];
+    
+    
+}
