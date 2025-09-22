d3950423d7ca15e6c3b7927e882d9c45d0a227f8
Archiviste-ken <shreyeshpachpor06@gmail.com>
2025-09-22 17:48:35 +0000

    map filter

---
+++ 05_iterations/eight.js
@@ -0,0 +1,48 @@
+// Reduce +++++++++++++++++++++++++++++++++++++++++++++
+const myNums = [1,2,3]
+
+// const myTotal =myNums.reduce(function (acc,currval){
+//     console.log(`acc is ${acc} and currval is ${currval}`);
+    
+//     return acc + currval
+// },0)  // 0 is initialising the accumulator since it can not proceed by itself naturally.
+
+
+// const myTotal = myNums.reduce((acc,currval) => {
+//             console.log(`acc is ${acc} and currval is ${currval}`);
+//             return acc + currval
+            
+// },0)
+
+// const myTotal = myNums.reduce ((acc,curr) => acc+curr,0)
+
+//console.log(myTotal);
+
+
+const shoppingCart =[
+    {
+        itemName: "js course",
+        price: 2999
+    },
+        {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+        {
+        itemName: "data science course",
+        price: 12999
+    },
+
+] 
+
+const priceTopay = shoppingCart.reduce((acc,item) => acc + item.price, 0)
+
+
+console.log(priceTopay);
+
+
+
